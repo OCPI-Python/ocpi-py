c6c18685d58b5fe7940fece5b90b68c8e57aa3cb
Leon <leon0824@gmail.com>
2025-10-26 22:42:20 +0800

    Add tests

--- tests/test_001_ocpiapp.py
+++ tests/test_001_ocpiapp.py
@@ -1,6 +1,7 @@
 from ocpi_py import OcpiApp
 
 
+
 class TestOcpiApp:
     def test_ocpi_app(self):
         app = OcpiApp()
---
+++ tests/test_002_ocpiapirouter.py
@@ -0,0 +1,8 @@
+from ocpi_py import OcpiAPIRouter
+
+
+
+class TestOcpiApiRouter:
+    def test_ocpi_api_router(self):
+        router = OcpiAPIRouter()
+        assert router
